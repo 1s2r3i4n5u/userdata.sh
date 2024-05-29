fefdf7ce4b685cf16bb09b094aceeab4b02378c9
1s2r3i4n5u <169056492+1s2r3i4n5u@users.noreply.github.com>
2024-05-29 11:14:04 +0530

    userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,7 +3,7 @@ Sudo yum install git -y
 Sudo yum install -y yum-utils
 Sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 Sudo yum install terraform -y
-Sudo git clone https://github.com/afzal-in/python-tf.git
+Sudo git clone https://github.com/1s2r3i4n5u/car-prediction.git
 cd car-prediction
 Terraform init
 Terraform plan
